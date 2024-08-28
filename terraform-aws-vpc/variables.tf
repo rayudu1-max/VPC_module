02a0e618ac50ea62b28aeaa85c278e1f36c1dd94
rayudu1-max <subbuhari999@gmail.com>
2024-08-28 08:58:26 +0530

    VPC module

---
+++ terraform-aws-vpc/variables.tf
@@ -0,0 +1,106 @@
+### project variables ###
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+  default = "dev"
+}
+
+variable "common_tags" {
+    type = map
+  
+}
+
+
+### VPC tags ###
+variable "cidr_block" {
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "enable_dns_hostnames" {
+    type = bool
+    default = true
+}
+
+variable "vpc_tags" {
+    type = map 
+    default = {}
+}
+
+### IGW ###
+variable "igw_tags" {
+    type = map 
+    default = {}
+}
+
+
+### public Subnets ###
+variable "public_subnet_cidrs" {
+    type = list 
+    validation {
+      condition = length(var.public_subnet_cidrs) == 2
+      error_message = "Please provide 2 valid public subnet CIDR"
+    }
+}
+
+variable "public_subnet_cidr_tags" {
+    type = map
+    default = {}
+}
+
+
+### private Subnets ###
+variable "private_subnet_cidrs" {
+    type = list 
+    validation {
+      condition = length(var.private_subnet_cidrs) == 2
+      error_message = "Please provide 2 valid private subnet CIDR"
+    }
+}
+
+variable "private_subnet_cidr_tags" {
+    type = map
+    default = {}
+}
+
+
+### database Subnets ###
+variable "database_subnet_cidrs" {
+    type = list 
+    validation {
+      condition = length(var.database_subnet_cidrs) == 2
+      error_message = "Please provide 2 valid database subnet CIDR"
+    }
+}
+
+variable "database_subnet_cidr_tags" {
+    type = map
+    default = {}
+}
+
+
+### nat gateway ###
+variable "nat_gateway_tags" {
+    type = map 
+    default = {}
+}
+
+### route table ###
+
+variable "public_route_table_tags" {
+    type = map 
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    type = map 
+    default = {}
+}
+
+variable "database_route_table_tags" { 
+    type = map 
+    default = {}
+}
