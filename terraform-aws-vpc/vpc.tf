2f84d9524ef4955e1b1889b770208a41df55b036
rayudu1-max <subbuhari999@gmail.com>
2024-08-28 12:14:48 +0530

    VPC module

--- terraform-aws-vpc/vpc.tf
+++ terraform-aws-vpc/vpc.tf
@@ -76,7 +76,7 @@ resource "aws_subnet" "database" {
 
 resource "aws_db_subnet_group" "default" {
   name       = "${local.resource_name}"
-  subnet_ids = aws.subnet.database[*].id
+  subnet_ids = aws_subnet.database[*].id
 
   tags = merge(
     var.common_tags,
--- terraform-vpc-test/outputs.tf
+++ terraform-vpc-test/outputs.tf
@@ -1,3 +1,24 @@
 /* output "azs_info" {
     value = module.vpc.azs
 } */
+
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
+
+output "database_subnet_ids" {
+  value = module.vpc.database_subnet_ids
+}
+
+output "database_subnet_group_id" {
+  value = module.vpc.database_subnet_group_id
+}
